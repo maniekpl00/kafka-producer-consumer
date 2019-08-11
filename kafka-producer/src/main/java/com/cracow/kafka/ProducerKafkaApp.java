18d7e4397550a91b9e598dc1ad5362d7e24aadb2
maniekpl00 <maniek11pl@gmail.com>
2019-08-11 23:26:56 +0200

    Delete after

--- kafka-producer/src/main/java/com/cracow/kafka/ProducerKafkaApp.java
+++ kafka-producer/src/main/java/com/cracow/kafka/ProducerKafkaApp.java
@@ -4,7 +4,10 @@ import com.cracow.kafka.config.KafkaConfig;
 import com.cracow.kafka.dto.SensorDto;
 import com.cracow.kafka.producer.ProducerKafkaCreator;
 import com.cracow.kafka.serializer.DeviceSerializer;
+import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -17,20 +20,61 @@ public class ProducerKafkaApp {
   }
 
   private static void run() {
+//    final Producer<Long, SensorDto> producer = ProducerKafkaCreator.build();
+//    final DeviceSerializer deviceSerializer = new DeviceSerializer();
+
+//    Runnable runnable = new Runnable() {
+//      @Override
+//      public void run() {
+//        final Producer<Long, SensorDto> producer = ProducerKafkaCreator.build();
+//        final DeviceSerializer deviceSerializer = new DeviceSerializer();
+//        for(int i = 0; i < 1; i++) {
+//          SensorDto sensorDto = new SensorDto("ca", "maniek", "channel1", "messages", new LinkedList<Double>() {{
+//            add(1D);
+//            add(721D);
+//          }}, Instant.now().toEpochMilli(), 4000);
+//
+//          final ProducerRecord<Long, SensorDto> record =
+//              new ProducerRecord<>(KafkaConfig.TOPIC_NAME, Long.valueOf(i), sensorDto);
+//          try {
+//            RecordMetadata metadata = producer.send(record).get();
+//            producer.send(record).get();
+//            String messageLog = String.format("Key: %s, partition: %s, offset: %s, value: %s",
+//                i, metadata.partition(), metadata.offset(), sensorDto);
+//            System.out.println(messageLog);
+//          } catch (InterruptedException e) {
+//            e.printStackTrace();
+//          } catch (ExecutionException e) {
+//            e.printStackTrace();
+//          }
+//        }
+//        producer.close();
+//      }
+//    };
+//    int count = 1;
+//    Thread [] threads = new Thread[count];
+//    for(int i = 0; i < count; i++) {
+//      threads[i] = new Thread(runnable);
+//      threads[i].start();
+//    }
+//
+
+
+
     final Producer<Long, SensorDto> producer = ProducerKafkaCreator.build();
     final DeviceSerializer deviceSerializer = new DeviceSerializer();
-
-    for(int i = 0; i < KafkaConfig.MESSAGE_COUNT; i++) {
-      String id = "id" + i;
-      String serialNumber = "SerialCHL" + i;
-      SensorDto sensorDto = new SensorDto(id, serialNumber, Instant.now().getEpochSecond());
+    for(int i = 0; i < 1; i++) {
+      SensorDto sensorDto = new SensorDto("ca", "maniek", "channel1", "messages", new LinkedList<Double>() {{
+        add(3D);
+        add(721D);
+      }}, Instant.now().toEpochMilli(), 5000);
 
       final ProducerRecord<Long, SensorDto> record =
           new ProducerRecord<>(KafkaConfig.TOPIC_NAME, Long.valueOf(i), sensorDto);
       try {
         RecordMetadata metadata = producer.send(record).get();
         String messageLog = String.format("Key: %s, partition: %s, offset: %s, value: %s",
-          i, metadata.partition(), metadata.offset(), sensorDto);
+            i, metadata.partition(), metadata.offset(), sensorDto);
         System.out.println(messageLog);
       } catch (InterruptedException e) {
         e.printStackTrace();
--- kafka-producer/src/main/java/com/cracow/kafka/dto/SensorDto.java
+++ kafka-producer/src/main/java/com/cracow/kafka/dto/SensorDto.java
@@ -1,52 +1,101 @@
 package com.cracow.kafka.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class SensorDto implements Serializable {
   private static final long serialVersionUID = 1L;
 
-  private String id;
-  private String serialNumber;
-  private Long eventTime;
+  private String tenant;
+  private String project;
+  private String channelId;
+  private String type;
+  private List<Double> values;
+  private double measureTimestampMilliseconds;
+  private double measureDurationMilliseconds;
 
-  public SensorDto() {
-  }
-
-  public SensorDto(String id, String serialNumber, Long eventTime) {
-    this.id = id;
-    this.serialNumber = serialNumber;
-    this.eventTime = eventTime;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getSerialNumber() {
-    return serialNumber;
-  }
-
-  public void setSerialNumber(String serialNumber) {
-    this.serialNumber = serialNumber;
-  }
-
-  public Long getEventTime() {
-    return eventTime;
-  }
-
-  public void setEventTime(Long eventTime) {
-    this.eventTime = eventTime;
+  public SensorDto(String tenant, String project, String channelId, String type,
+      List<Double> values, double measureTimestampMilliseconds,
+      double measureDurationMilliseconds) {
+    this.tenant = tenant;
+    this.project = project;
+    this.channelId = channelId;
+    this.type = type;
+    this.values = values;
+    this.measureTimestampMilliseconds = measureTimestampMilliseconds;
+    this.measureDurationMilliseconds = measureDurationMilliseconds;
   }
 
   @Override
   public String toString() {
     return "SensorDto{" +
-        "id='" + id + '\'' +
-        ", serialNumber='" + serialNumber + '\'' +
+        "tenant='" + tenant + '\'' +
+        ", project='" + project + '\'' +
+        ", channelId='" + channelId + '\'' +
+        ", type='" + type + '\'' +
+        ", values=" + values +
+        ", measureTimestampMilliseconds=" + measureTimestampMilliseconds +
+        ", measureDurationMilliseconds=" + measureDurationMilliseconds +
         '}';
+  }
+
+  public static long getSerialVersionUID() {
+    return serialVersionUID;
+  }
+
+  public String getTenant() {
+    return tenant;
+  }
+
+  public void setTenant(String tenant) {
+    this.tenant = tenant;
+  }
+
+  public String getProject() {
+    return project;
+  }
+
+  public void setProject(String project) {
+    this.project = project;
+  }
+
+  public String getChannelId() {
+    return channelId;
+  }
+
+  public void setChannelId(String channelId) {
+    this.channelId = channelId;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public List<Double> getValues() {
+    return values;
+  }
+
+  public void setValues(List<Double> values) {
+    this.values = values;
+  }
+
+  public double getMeasureTimestampMilliseconds() {
+    return measureTimestampMilliseconds;
+  }
+
+  public void setMeasureTimestampMilliseconds(double measureTimestampMilliseconds) {
+    this.measureTimestampMilliseconds = measureTimestampMilliseconds;
+  }
+
+  public double getMeasureDurationMilliseconds() {
+    return measureDurationMilliseconds;
+  }
+
+  public void setMeasureDurationMilliseconds(double measureDurationMilliseconds) {
+    this.measureDurationMilliseconds = measureDurationMilliseconds;
   }
 }
